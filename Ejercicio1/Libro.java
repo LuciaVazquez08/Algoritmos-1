75729a0cf605824cd041d7f8be12564e72522e8c
Lucia Vazquez <luchi08vazquez@gmail.com>
2023-09-21 11:36:55 -0300

    libro

--- Ejercicio1/Libro.java
+++ Ejercicio1/Libro.java
@@ -1,6 +1,6 @@
 package Ejercicio1;
-
 import java.time.LocalDate;
+
 public class Libro {
     // Atributos
     String titulo;
@@ -17,7 +17,18 @@ public class Libro {
         this.titulo = titulo;
         this.autor = autor;
         this.publicacion = publicacion;
-    
-    
+    }
+
+    public static void main(String[] args) {
+        Libro FW = new Libro("Fourth Wing", "Rebecca Yarros");
+        Libro BL = new Libro("Book Lover", "Emily Henry", 2022);
+
+        System.out.println("Titulo:" + FW.titulo);
+        System.out.println("Autor:" + FW.autor);
+        System.out.println("Año de Publicacion:" + FW.publicacion);
+
+        System.out.println("Titulo:" + BL.titulo);
+        System.out.println("Autor:" + BL.autor);
+        System.out.println("Año de Publicacion:" + BL.publicacion);
     }
 }
