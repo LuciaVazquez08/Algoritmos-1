75dd4b41edcb0ecfb5aed80b7b65eb4deb90bfc9
Lucia Vazquez <luchi08vazquez@gmail.com>
2023-09-21 10:11:16 -0300

    agregue mi programa para testear

---
+++ MiPrograma.java
@@ -0,0 +1,14 @@
+//pasos para hacer un proyecto
+
+// 1- escribir programa
+public class MiPrograma {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Mundo!");
+    }  
+}
+
+// 2- compilar y ejecutar
+    // abrir terminal
+    // ir a carpeta del proyecto
+    // compilar con : javac MiPrograma.java ; genera archivo .class con bycode
+    // ejecutar con: java MiPrograma
