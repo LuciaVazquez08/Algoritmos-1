83cdf5405b5d61d50966afa1fc69fa81b3b8bda4
Lucia Vazquez <luchi08vazquez@gmail.com>
2023-09-22 11:32:28 -0300

    conversor

--- ConversorTemperatura.java
+++ ConversorTemperatura.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class ConversorTemperatura {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese la unidad de temperatura (C/F): ");
+        String unidad = scanner.nextLine();
+
+        System.out.print("Ingrese la temperatura: ");
+        double tempinc = scanner.nextDouble();
+
+        if (unidad.equals("C")) {
+            double tempfin = (tempinc * 1.8) + 32;
+            System.out.println(tempfin);
+        } else if (unidad.equals("F")) {
+            double tempfin = (tempinc - 32) / 1.8;
+            System.out.println(tempfin);
+        } else {
+            System.out.println("Ingrese una unidad valida");
+        }
+        scanner.close();
+    }
 }
