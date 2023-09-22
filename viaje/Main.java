93c02fb292935f4e2e9169a41fd9920ac1377cdf
Lucia Vazquez <luchi08vazquez@gmail.com>
2023-09-22 15:18:26 -0300

    test

--- viaje/Main.java
+++ viaje/Main.java
@@ -6,6 +6,11 @@ public class Main {
     
     Viaje barilocheDiesel = new Diesel(bariloche, 10, 500);
     System.out.println(barilocheDiesel.tiempoDemora());
-    
+
+    Viaje barilocheElectrico = new Electrico(bariloche, 10, 500);
+    System.out.println(barilocheElectrico.tiempoDemora());
+
+    Viaje barilocheAltaVeloc = new AltaVelocidad(bariloche, 10, 500);
+    System.out.println(barilocheAltaVeloc.tiempoDemora());
     }
 }
