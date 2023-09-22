0c9a7a7334472f00e35a02af1e97d4c16d4521e4
Lucia Vazquez <luchi08vazquez@gmail.com>
2023-09-22 11:52:58 -0300

    palabras

--- ContadorPalabras.java
+++ ContadorPalabras.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class ContadorPalabras {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese una frase: ");
+        String frase = scanner.nextLine();
+
+        System.out.print("Ingrese un separador: ");
+        String separadorInput = scanner.nextLine();
+
+        char separador = separadorInput.isEmpty() ? ' ' : separadorInput.charAt(0);
+
+        String[] palabras = frase.split(String.valueOf(separador));
+
+        int cantidadPalabras = palabras.length;
+
+        System.out.println("Cantidad de palabras: " + cantidadPalabras);
+
+        scanner.close();
+    }    
 }
