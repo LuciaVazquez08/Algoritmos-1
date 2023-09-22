8559f1e3b1eb2d76e014412f4a5f6ca2dfab2065
Lucia Vazquez <luchi08vazquez@gmail.com>
2023-09-22 10:02:22 -0300

    ent-sal

--- Ejercicio4/Entrada/EntradaConsola.java
+++ Ejercicio4/Entrada/EntradaConsola.java
@@ -1,21 +1,21 @@
-package Ejercicio4.Entrada;
-
+package Ejercicio4.entrada;
 import java.util.Scanner;
+import Ejercicio4.salida.SalidaConsola;
+
 
 public class EntradaConsola {
-
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Por favor, ingrese un número entero como argumento.");
         }
-
         int entrada = Integer.parseInt(args[0]);
+        SalidaConsola.mostrar("Por favor ingrese " + entrada + " entradas de texto");
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Por favor ingrese " + entrada + " entradas de texto");
 
         for (int i = 0; i < entrada; i++) {
             String laentrada = scanner.nextLine();
-            System.out.println("Entrada " + (i + 1) + ": " + laentrada);
+            SalidaConsola.mostrar("Entrada " + (i + 1) + ": " + laentrada);
         }
 
         scanner.close();
