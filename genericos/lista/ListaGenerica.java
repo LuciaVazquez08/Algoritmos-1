7f3aee1f4f7395a3b3b4471e721dcae8c5e93e5f
Lucia Vazquez <luchi08vazquez@gmail.com>
2023-10-07 16:17:18 -0300

    lista generica

--- genericos/lista/ListaGenerica.java
+++ genericos/lista/ListaGenerica.java
@@ -2,51 +2,65 @@ package genericos.lista;
 
 import java.util.AbstractList;
 import java.util.Arrays;
-import java.util.Collection;
+
 
 public class ListaGenerica<E> extends AbstractList<E>{
-    private static final int insize = 10;
-    private E[] elementos;
+    private static final int CAPACIDAD_INICIAL = 10;
+    private Object[] elementos;
     private int size;
 
     public ListaGenerica() {
-        E[] elementos = new E[insize];
+        elementos = new Object[CAPACIDAD_INICIAL];
         size = 0;
     }
 
     @Override
-    //public E set(int index, E element) {
-        //elementos[index] = element;
-    //}
+    public E set(int index, E element) {
+        elementos[index] = element;
+        return element;
+    }
     
     @Override
-    public void add(int index, E element){
+    public boolean add(E element){
         if (size == elementos.length) {
             int nuevaCapacidad = elementos.length * 2;
             elementos = Arrays.copyOf(elementos, nuevaCapacidad);
         }
-        System.arraycopy(elementos, index, elementos, index + 1, size - index);
-        elementos[index] = element;
+        set(size, element);
+        size++;
+        return true;
+    }
+
+    @Override
+    public void add(int index, E element){
+        set(index, element);
         size++;
     }
     
     @Override
     public E remove(int index) {
-        E elementoRemovido = (E) elementos[index];
-        int elementosParaMover = size - index - 1;
-        if (elementosParaMover > 0) {
-            System.arraycopy(elementos, index + 1, elementos, index, elementosParaMover);
+        E elementoRemovido = get(index);
+        int i = 0;
+        boolean encontrado = false;
+        while (i < size - 1) {
+            if (encontrado || index == i) {
+                encontrado = true;
+                elementos[i] = elementos[i + 1];
+            }
+            i++;
         }
-        elementos[size - 1] = null; 
-        size--;
+        if (encontrado || index == size - 1) {
+            elementos[i] = null;
+            size--;
+        }
         return elementoRemovido;
     }
     
-    // se agregan como metodos porqeu sino se rompe todo
+    // se agregan como metodos porque sino se rompe todo
     @Override
     public E get(int index) {
         if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Índice fuera de rango");
+            throw new IndexOutOfBoundsException(index);
         }
         return (E) elementos[index];
     }
@@ -56,11 +70,19 @@ public class ListaGenerica<E> extends AbstractList<E>{
         return size;
     }
 
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addAll'");
+    public static void main(String[] args) {
+        ListaGenerica<Integer> lista = new ListaGenerica<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(2, 3);
+        lista.set(0, 10);
+        lista.remove(0);
+        for(int i : lista) {
+            System.out.println(i);
+        }
     }
+
+   
 
     
 
