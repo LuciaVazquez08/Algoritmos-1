da63d2677738dfa8bbf05a5efbaca1d711eb570f
Lucia Vazquez <131535537+LuciaVazquez08@users.noreply.github.com>
2024-03-09 18:56:43 -0300

    final organization

--- tpfinal/Celda.java
+++
@@ -1,9 +0,0 @@
-package tpfinal;
-
-public abstract class Celda {
-    protected Object contenido;
-    
-    public abstract void getContenido();
-    public abstract void setContenido();
-    public abstract void addContenido();
-}
